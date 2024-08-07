b9626dd6185c8b0c16d954335da3fa8a44959c34
Nabin Singh <nabin@skilluplabs.com.au>
2024-08-07 19:25:42 +1000

    Removed jade view

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'To Do' });
+  res.status(200).send('To Do API');
 });
 
 export default router;
