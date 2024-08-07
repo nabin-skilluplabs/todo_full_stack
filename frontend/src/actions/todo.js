09b421c98f90956314b72ab86c5ea77cd0a67578
Nabin Singh <nabin@skilluplabs.com.au>
2024-08-07 20:16:27 +1000

    Updated API_ENDPOINT to use server url

--- frontend/src/actions/todo.js
+++ frontend/src/actions/todo.js
@@ -1,4 +1,5 @@
-const API_ENDPOINT = "http://localhost:3000";
+const API_ENDPOINT = (location.hostname === 'localhost') ? "http://localhost:3000" : "https://todo-backend-seven-nu.vercel.app";
+
 export async  function  getAllToDos() {
     const response = await fetch(`${API_ENDPOINT}/v1/todos?limit=10`);
     const data = await response.json();
